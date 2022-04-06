9535d85f206108cc8d04103d4abb90a803f543b6
Tanmay Narendra Chandane <33379183+tanmaychandane@users.noreply.github.com>
2022-04-06 16:23:20 +0530

    Create select_operator.sql

---
+++ MySQL/select_operator.sql
@@ -0,0 +1,23 @@
+USE sql_store; 
+
+  
+
+SELECT first_name, last_name, points, points * 10 + 100 AS 'discount factor' 
+
+FROM customers; 
+
+  
+
+select * FROM sql_store.customers;  
+
+  
+
+SELECT DISTINCT state 
+
+FROM customers; 
+
+  
+
+SELECT name, unit_price, unit_price * 1.1 AS 'new price'  
+
+FROM sql_store.products; 
