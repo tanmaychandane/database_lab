218dad737ab565a50f518eb0468fc1f114c8de8f
Tanmay Narendra Chandane <33379183+tanmaychandane@users.noreply.github.com>
2022-04-11 16:12:18 +0530

    Create innner_join.sql

---
+++ MySQL/innner_join.sql
@@ -0,0 +1,9 @@
+USE sql_store;
+
+SELECT order_id, o.customer_id, first_name, last_name
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id;
+
+SELECT order_id, oi.product_id, quantity, oi.unit_price
+FROM order_items oi
+JOIN products p ON oi.product_id = p.product_id;
